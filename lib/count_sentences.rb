926bae27e8e9721bd8c601ca12bec88ae9fdc6b6
jw20191n <clairejw1015@gmail.com>
2019-08-27 17:45:11 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    @array_of_sentence = self.split(/\?|\.|!/)
+    @array_of_sentence.reject!{|item| item.empty? }
+    @array_of_sentence.length
   end
+
 end
